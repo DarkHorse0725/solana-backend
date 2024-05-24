f06aa352c04e4125cbce7cabae61d27ae39a7a4f
darkhorse0725 <darktop0112@gmail.com>
2024-05-25 01:25:49 +0900

    signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { SignupDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signup')
+  async signup(@Body() body: SignupDto) {
+    const signupUser = await this.authService.signUp(body);
+    return signupUser;
+  }
 }
